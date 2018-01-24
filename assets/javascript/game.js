e734cceb536f855b2dcb1d00c38c5bae57b9344d
gypsyelliot <elliot@elliotreed.net>
2018-01-23 22:21:48 -0700

    start

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Start game
+// Get random number between 19 - 120
+// display randon number
+// get crystal values 1 - 12
+// Click event for crystals
+// Add crystal value to crystal total
+
+// If total under target, keep playing
+// If total equals target, you win
+// If total over target, you lose
