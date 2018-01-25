a460f9ff51419bda8e74c6ea7470d041c1b7451f
gypsyelliot <elliot@elliotreed.net>
2018-01-25 14:30:55 -0700

    from class thursday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,19 @@
 // Start game
+var wins = 0;
+var losses = 0;
+
+
+function playGame() {
+
+var RandomNmuber = randomIntFromInterval(19, 120)
+
+
+
+
+
+
+
+}
 // Get random number between 19 - 120
 // display randon number
 // get crystal values 1 - 12
@@ -8,3 +23,7 @@
 // If total under target, keep playing
 // If total equals target, you win
 // If total over target, you lose
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min +1) + min)
+}
+playGame();
